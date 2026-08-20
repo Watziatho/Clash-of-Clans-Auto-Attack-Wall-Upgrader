f90d6e57fbeabc66d66356636bcfc8e3ff320734
Watziatho <hikamantasyard@gmail.com>
2026-08-20 14:49:34 +0700

    Fix: Subprocess Popen.__init__ hook for full asyncio and apscheduler compatibility in PyInstaller

--- src/main.py
+++ src/main.py
@@ -3,14 +3,14 @@ import sys
 # Suppress all background console window creation on Windows (adb.exe, minitouch, etc.)
 if sys.platform == "win32":
     import subprocess
-    _orig_popen = subprocess.Popen
-    def _silent_popen(*args, **kwargs):
+    _orig_popen_init = subprocess.Popen.__init__
+    def _silent_popen_init(self, *args, **kwargs):
         if "creationflags" not in kwargs:
             kwargs["creationflags"] = 0x08000000  # CREATE_NO_WINDOW
         else:
             kwargs["creationflags"] |= 0x08000000
-        return _orig_popen(*args, **kwargs)
-    subprocess.Popen = _silent_popen
+        _orig_popen_init(self, *args, **kwargs)
+    subprocess.Popen.__init__ = _silent_popen_init
 
 from multiprocessing import freeze_support
 from launch import launch
--- src/utils.py
+++ src/utils.py
@@ -5,14 +5,14 @@ from functools import lru_cache
 
 # Suppress all background console window creation on Windows (adb.exe, minitouch, etc.)
 if sys.platform == "win32":
-    _orig_popen = subprocess.Popen
-    def _silent_popen(*args, **kwargs):
+    _orig_popen_init = subprocess.Popen.__init__
+    def _silent_popen_init(self, *args, **kwargs):
         if "creationflags" not in kwargs:
             kwargs["creationflags"] = 0x08000000  # CREATE_NO_WINDOW
         else:
             kwargs["creationflags"] |= 0x08000000
-        return _orig_popen(*args, **kwargs)
-    subprocess.Popen = _silent_popen
+        _orig_popen_init(self, *args, **kwargs)
+    subprocess.Popen.__init__ = _silent_popen_init
 
 try:
     import configs
